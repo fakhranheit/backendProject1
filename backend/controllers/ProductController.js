974d0d2212abb0901536ab1b490f6e108c4b77bc
fakhranheit <fakhranwahyu@gmail.com>
2020-03-02 16:29:32 +0700

    2maret2020

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -12,6 +12,7 @@ module.exports = {
                 const { image } = req.files
                 const imagePath = image ? path + "/" + image[0].filename : null
                 const data = JSON.parse(req.body.data)
+                console.log(data)
                 data.Foto = imagePath
                 if (err) {
                     fs.unlinkSync("./public" + imagePath);
@@ -28,11 +29,8 @@ module.exports = {
                             error: err.message
                         })
                     }
-                    // console.log('masuk sini')
-                    // console.log(res2)
-                    // return res.status(200).send({ status: "OK" })
-                    sql = 'select gr.namaGenre,gm.namaGame,gm.deskripsi,gm.foto from genre gr join game gm on gm.genreId=gr.id'
                     // sql = 'select * from game'
+                    sql = 'select gr.namaGenre,gm.namaGame,gm.deskripsi,gm.foto from genre gr join game gm on gm.genreId=gr.id'
                     mysqldb.query(sql, (err1, result1) => {
                         if (err1) res.status(500).send(err1)
                         res.status(200).send({ dataGame: result1 })
@@ -45,6 +43,26 @@ module.exports = {
             res.status(200).send({ status: error, message: 'there is a problem with the uploader' })
         }
     },
+    postGenre: (req, res) => {
+        console.log('masuk post genre')
+        var { namaGenre } = req.body
+        var data = {
+            namaGenre
+        }
+        var sql = `insert into genre set ?`
+        mysqldb.query(sql, data, (err, res1) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).send({ status: err })
+            }
+            sql = 'select * from genre'
+            mysqldb.query(sql, (err1, result1) => {
+                if (err1) res.status(500).send(err1)
+                res.status(200).send({ result1 })
+            })
+        })
+
+    },
     getGenre: (req, res) => {
         var sql = 'select * from genre'
         mysqldb.query(sql, (err, res1) => {
@@ -53,7 +71,7 @@ module.exports = {
         })
     },
     getGame: (req, res) => {
-        var sql = 'select gr.namaGenre,gm.namaGame,gm.deskripsi,gm.foto,gm.id from genre gr join game gm on gm.genreId=gr.id'
+        var sql = 'select gr.namaGenre,gm.namaGame,gm.deskripsi,gm.foto,gm.id,gm.harga,gm.tanggalUpload from genre gr join game gm on gm.genreId=gr.id'
         mysqldb.query(sql, (err, res1) => {
             if (err) {
                 return res.status(500).send(err)
@@ -98,5 +116,63 @@ module.exports = {
             });
         })
 
+    },
+    editGame: (req, res) => {
+        var { selectedidEdit } = req.params
+        console.log('id', selectedidEdit)
+        var sql = `select * from game where id=${selectedidEdit}`
+        mysqldb.query(sql, (err, res1) => {
+            if (err) return res.status(500).send(err)
+            // console.log(res1)
+            // return res.status(200).send(res1)
+            if (res1.length) {
+                const path = "/game/images"
+                const upload = uploader(path, 'GAME').fields([{ name: 'image' }])
+
+                upload(req, res, (err) => {
+                    if (err) return res.status(500).json({ message: 'server error' })
+                    const { image } = req.files
+                    const imagePath = image ? path + "/" + image[0].filename : null
+                    console.log('ini reqbody', req.body.data)
+                    const data = JSON.parse(req.body.data)
+
+                    try {
+                        if (imagePath) {
+                            data.Foto = imagePath
+                        }
+                        sql = `update game set ? where id=${selectedidEdit}`
+                        mysqldb.query(sql, data, (err, res2) => {
+                            if (err) {
+                                if (imagePath) {
+                                    fs.unlinkSync(`./public${imagePath}`)
+                                }
+                                return res2.status(500).json({ message: 'server error' })
+                            }
+                            if (imagePath) {
+                                if (res1[0].Foto) {
+                                    fs.unlinkSync(`./public${res1[0].Foto}`)
+                                }
+                            }
+
+                            sql = `select * from game`
+                            mysqldb.query(sql, (err, res3) => {
+                                if (err) return res.status(500).send(err)
+                                return res.status(200).send(res3)
+                            })
+                        })
+                    } catch (error) {
+                        console.log(error)
+                    }
+                })
+
+            }
+        })
+    },
+    getLatestGame: (req, res) => {
+        var sql = 'select * from game gm join genre gr on gm.genreId=gr.id order by tanggalUpload DESC LIMIT 0, 3;'
+        mysqldb.query(sql, (err, res1) => {
+            if (err) return res.status(500).send(err)
+            return res.status(200).send(res1)
+        })
     }
 }
--- backend/routers/Gamerouter.js
+++ backend/routers/Gamerouter.js
@@ -2,9 +2,12 @@ const express = require("express");
 const { ProductController } = require("../controllers");
 const router = express.Router();
 
+router.put('/editgame/:selectedidEdit', ProductController.editGame)
 router.post('/addGame', ProductController.postGame)
+router.post('/addgenre', ProductController.postGenre)
 router.get('/getgenre', ProductController.getGenre)
 router.get('/getgame', ProductController.getGame)
+router.get('/getlatest', ProductController.getLatestGame)
 router.delete('/deletegame/:selectedId', ProductController.deleteGame)
 
 
